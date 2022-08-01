d14589229864080d1c219f6de98293b43cb1d70f
PoliakPavel <pashkapol27@gmail.com>
2022-08-01 10:52:24 +0300

    none

---
+++ taskmanager/main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import TaskForm
+
+
+# Create your views here.
+def index(request):
+    task = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title':'главная страница', 'tasks':task})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error =''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма заполнена неверно'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
